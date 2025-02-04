ae97081e2fc33701294d6763b810200bd6045f3c
root <root@ip-172-31-9-200.ap-south-1.compute.internal>
2025-02-04 09:32:02 +0000

    new commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
-public class HalloWorld {
+public class HelloWorld {
 public static void main(String[] args) {
-System.out.println("Hello, borld");
+System.out.println("Hello, World");
 System.out.println("this is madhu");
-System.out.println("HI 45 batch devaps"):
+System.out.println("HI 43 batch devops");
 }
 }
+
